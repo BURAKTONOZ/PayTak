41e038bee30fec2edd76a76ed52e2b694f28c630
BURAKTONOZ <buraktonoz@gmail.com>
2026-08-25 18:51:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain, screen } = require('electron')
 const path = require('path')
 
 function createWindow () {
+  // Ekranın tam boyutlarını al
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize
+
   const win = new BrowserWindow({
-    width: 1000,
-    height: 800,
+    width: width,
+    height: height,
     transparent: true, // Şeffaf arka plan
-    frame: false,      // Çerçevesiz (Kapatma, küçültme tuşları yok)
-    alwaysOnTop: true, // Her zaman açık olan pencerelerin üstünde kalır
+    frame: false,      // Çerçevesiz
+    alwaysOnTop: true, // Her zaman üstte
+    skipTaskbar: true, // Görev çubuğunda görünmesin (tam maskot hissi)
     resizable: false,
     webPreferences: {
       nodeIntegration: true,
@@ -15,7 +19,19 @@ function createWindow () {
     }
   })
 
+  // Başlangıçta tüm pencereyi tıklamalara karşı "Geçirgen" yap (Arkaya tıklansın)
+  win.setIgnoreMouseEvents(true, { forward: true })
+
   win.loadFile('index.html')
+
+  // index.html'den gelen sinyale göre farenin geçirgenliğini aç/kapat
+  ipcMain.on('set-ignore-mouse-events', (event, ignore) => {
+    const webContents = event.sender
+    const currentWin = BrowserWindow.fromWebContents(webContents)
+    if (currentWin) {
+        currentWin.setIgnoreMouseEvents(ignore, { forward: true })
+    }
+  })
 }
 
 app.whenReady().then(createWindow)
